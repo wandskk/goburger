9302bb962ac0d8b0cc2ded213f996617667d9348
wandskk <wandersonkenedy13@gmail.com>
2021-10-18 03:02:49 -0300

    Adicionado bootstrap ao projeto

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
