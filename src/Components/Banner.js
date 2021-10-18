82a39d20a5ce24d35a9a085d964c27908d41acbe
wandskk <wandersonkenedy13@gmail.com>
2021-10-18 12:28:08 -0300

    Banner da home implementado

---
+++ src/Components/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Banner.module.css';
+
+const Banner = () => {
+  return (
+    <section className={styles.banner}>
+      <div className={styles.title}>Faça seu Hamburger</div>
+    </section>
+  );
+};
+
+export default Banner;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Banner from '../Components/Banner';
+import styles from './Home.module.css';
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Banner />
+      <h1 className={styles.title}>Monte o seu burger:</h1>
+    </div>
+  );
 };
 
 export default Home;
