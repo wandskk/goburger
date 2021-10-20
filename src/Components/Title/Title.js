bdbf8b918ef4b69c171f7cd171a156181a04fd99
wandskk <wandersonkenedy13@gmail.com>
2021-10-20 14:10:57 -0300

    Ajustes layout home

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -2,7 +2,11 @@ import React from 'react';
 import styles from './Title.module.css';
 
 const H1 = ({ text }) => {
-  return <h1 className={styles.title}>{text}</h1>;
+  return (
+    <div className="offset-4 col-4 offset-4">
+      <h1 className={styles.title}>{text}</h1>
+    </div>
+  );
 };
 
 export default H1;
